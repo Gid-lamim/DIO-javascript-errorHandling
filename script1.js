d07a7b5722503f139291300ca9e759740e412e2b
Gid.Lamim <gid.lamim@gmail.com>
2022-06-30 10:10:51 -0300

    add second validation
    the first oarameter must be an array. It's going to check whether the arr is an array. if not, it'll throw a TypeError

--- script1.js
+++ script1.js
@@ -4,7 +4,8 @@ function validaArray (arr, num){
     //se os parâmetros não tiverem sido enviados, lance um erro do tipo ReferenceError
     if(!arr || !num) throw new ReferenceError("Envie os parâmetros");
 
-  
+    //se o array não for um objeto, iremos lançar um TypeError.
+    if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object.");
 }
 
-console.log(validaArray(2));
+console.log(validaArray(2,2));
