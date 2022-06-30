8f7a9032508588deb9111be964cb3153294cad94
Gid.Lamim <gid.lamim@gmail.com>
2022-06-30 10:06:12 -0300

    add first valifation
    if one of the parameters are empty, throw a ReferenceError

--- script1.js
+++ script1.js
@@ -1,5 +1,10 @@
 //criar uma função que receba um array e um número como parâmetros.
 
 function validaArray (arr, num){
-    
+    //se os parâmetros não tiverem sido enviados, lance um erro do tipo ReferenceError
+    if(!arr || !num) throw new ReferenceError("Envie os parâmetros");
+
+  
 }
+
+console.log(validaArray(2));
