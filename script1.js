3b45b1943b2fbf73fbc8c69c4f697d07d1ea9a4a
Gid.Lamim <gid.lamim@gmail.com>
2022-06-30 10:20:13 -0300

    add third validation
    it checks whether the second parameter is indeed a number. If it's not, it'll throw a TypeError.

--- script1.js
+++ script1.js
@@ -5,7 +5,11 @@ function validaArray (arr, num){
     if(!arr || !num) throw new ReferenceError("Envie os parâmetros");
 
     //se o array não for um objeto, iremos lançar um TypeError.
+    //   !==   -> significa diferente de
     if(typeof arr !== 'object') throw new TypeError("Array precisa ser do tipo object.");
+
+    //se o número não for do tipo number, lance outro TypeError
+    if (typeof num !== 'number') throw new TypeError("O segundo parâmetro precisa ser do tipo number");
 }
 
-console.log(validaArray(2,2));
+console.log(validaArray([2],"a"));
